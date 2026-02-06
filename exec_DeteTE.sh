81b8bc00cd937d60522ab1a7310d22b656607c64
laurelamothe <119611850+laurelamothe@users.noreply.github.com>
2026-02-06 15:24:08 +0100

    Update exec_DeteTE.sh

--- exec_DeteTE.sh
+++ exec_DeteTE.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch -o . -J "" DetecTE_master.sh --fasta test.fasta --database db_test.fasta --assembler TRINITY --outdir . --dataset_name ""
+sbatch -o "report.out" -J "" DetecTE_master.sh --fasta test.fasta --database db_test.fasta --assembler TRINITY --outdir . --dataset_name ""
